5e199ea770b9150805c3e6525a8ec19046969744
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2022-03-19 09:36:04 +0100

    Get list of attendees

--- meetup_tracker/meetup.py
+++ meetup_tracker/meetup.py
@@ -24,11 +24,8 @@ class Meetup:
             events = self._get_events(group)
             for event in events[:2]:
                 print(f"Event: {event}")
-
-                self._driver.get(event)
-
-                if self._is_event_cancelled():
-                    continue
+                attendees = self._get_attendees(event)
+                print(attendees)
 
     def _login(self, url):
         self._driver.get(url)
@@ -63,6 +60,18 @@ class Meetup:
         events_list = self._driver.find_element(by=By.CLASS_NAME, value="list--infinite-scroll")
         return [event.find_element(by=By.TAG_NAME, value="a").get_attribute("href")
                 for event in events_list.find_elements(by=By.CLASS_NAME, value="card")]
+
+    def _get_attendees(self, event_url):
+        self._driver.get(event_url)
+
+        if self._is_event_cancelled():
+            return
+
+        self._driver.get(event_url + "attendees/")
+        WebDriverWait(self._driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "attendees-list")))
+        attendees_list = self._driver.find_element(by=By.CLASS_NAME, value="attendees-list")
+        attendees = attendees_list.find_elements(by=By.CLASS_NAME, value="attendee-item")
+        return [attendee.text.split("\n")[0] for attendee in attendees]
 
     def _is_event_cancelled(self):
         WebDriverWait(self._driver, 30) \
