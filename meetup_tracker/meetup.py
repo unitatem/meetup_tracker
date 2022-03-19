2804fb0f5edcd6664e4d59342908d56e26a8ff34
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2022-03-19 01:44:38 +0100

    Fix stale reference bug

--- meetup_tracker/meetup.py
+++ meetup_tracker/meetup.py
@@ -19,13 +19,13 @@ class Meetup:
 
         groups = self._get_groups()
         for group in groups[:1]:
-            print(f"Group name: {group.text}")
-            group.click()
+            print(f"Group: {group}")
+            self._driver.get(group)
 
             events = self._get_events()
             for event in events[:2]:
-                print("Event name: " + event.text.split("\n")[0])
-                event.click()
+                print(f"Event: {event}")
+                self._driver.get(event)
 
                 if self._is_event_cancelled():
                     continue
@@ -49,7 +49,7 @@ class Meetup:
 
         groups_list = self._driver.find_element_by_id("my-meetup-groups-list")
         groups = groups_list.find_elements(by=By.CLASS_NAME, value="D_name")
-        return [group.find_element(by=By.TAG_NAME, value="a") for group in groups]
+        return [group.find_element(by=By.TAG_NAME, value="a").get_attribute("href") for group in groups]
 
     def _get_events(self):
         WebDriverWait(self._driver, 30) \
@@ -60,7 +60,8 @@ class Meetup:
         WebDriverWait(self._driver, 30) \
             .until(EC.presence_of_element_located((By.CLASS_NAME, "list--infinite-scroll")))
         events_list = self._driver.find_element(by=By.CLASS_NAME, value="list--infinite-scroll")
-        return events_list.find_elements(by=By.CLASS_NAME, value="card")
+        return [event.find_element(by=By.TAG_NAME, value="a").get_attribute("href")
+                for event in events_list.find_elements(by=By.CLASS_NAME, value="card")]
 
     def _is_event_cancelled(self):
         WebDriverWait(self._driver, 30) \
